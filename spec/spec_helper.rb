5a5fbe7cd4309b97c1d7b9f4fba478222f861a23
Otis <altispec@gmail.com>
2015-09-19 04:58:29 +0800

    Fix coding style

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f }
+  Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
 
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
