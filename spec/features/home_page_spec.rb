6e9450de9e495aa6d06a33b32b405ccd30b08ec3
Juanito Fatas <katehuang0320@gmail.com>
2015-09-19 11:10:30 +0800

    We have infer spec type location in rails_helper.rb

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Home page", type: :feature do
+RSpec.describe "Home page" do
   scenario "visit" do
     visit "/"
     expect(page).to have_content "Welcome to Ruby Taiwan"
