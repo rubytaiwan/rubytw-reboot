50bda73cec507f6cecf2bcacfff6379dab3bf2ac
Juanito Fatas <katehuang0320@gmail.com>
2015-09-19 11:24:36 +0800

    Change home page scenario description

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "Home page" do
-  scenario "visit" do
+  scenario "can access" do
     visit root_path
 
     expect(page).to have_content "Welcome to Ruby Taiwan"
