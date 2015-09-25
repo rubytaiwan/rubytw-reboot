23a6bb297d0df1c4d4d33ea93e690fd3899f983f
Juanito Fatas <katehuang0320@gmail.com>
2015-09-26 01:20:14 +0800

    Add a simple spec make sure markdown renders correctly
    Closes #46

---
+++ spec/features/code_of_conduct_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.feature "Code of Conduct page" do
+  scenario "markdown renders correctly" do
+    visit code_of_conduct_path
+
+    expect(page).to have_css "h1", text: "Contributor Code of Conduct"
+  end
+end
